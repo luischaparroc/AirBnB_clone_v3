6c5e823882d38f8febd6e4df16036b97b8aec8f7
luischaparroc <lfchaparro@outlook.com>
2020-01-28 13:59:00 -0500

    [Feature]: adding /stats route in Blueprint & Flask Modified: api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,22 @@
 """ Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models.city import City
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """ Returns JSON """
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
