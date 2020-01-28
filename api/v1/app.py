d225ef50d7c5b43f3c1af8a49ce2bf437255d872
Alejandro Mogollon Medina <935@holbertonschool.com>
2020-01-28 19:15:13 +0000

    [Feature]: created a handler for 404 errors that returns a JSON- formatted 404 status code response. Modified: api/v1/app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ Script that imports a Blueprint and runs Flask """
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
-
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -14,6 +13,11 @@ app.register_blueprint(app_views)
 def teardown_session(exception):
     """ Closes storage session """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == '__main__':
