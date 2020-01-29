99907018635cb30188fc81cd951b8e53d1a06a6d
luischaparroc <lfchaparro@outlook.com>
2020-01-29 18:47:01 -0500

    [Fix]: does not remove password when is stored in disk

--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """ holds class User"""
 import models
 from models.base_model import BaseModel, Base
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -26,4 +27,8 @@ class User(BaseModel, Base):
 
     def __init__(self, *args, **kwargs):
         """initializes user"""
+        p = hashlib.md5()
+        passw = kwargs['password']
+        p.update(str.encode(passw))
+        self.password = p.hexdigest()
         super().__init__(*args, **kwargs)
